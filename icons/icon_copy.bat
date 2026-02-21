ad16c7a92be5c1d477558580e5fc05970e92d267
nobu5129maki-crypto <nobu5129maki@gmail.com>
2026-02-21 19:12:55 +0900

    Add files via upload

---
+++ icons/icon_copy.bat
@@ -0,0 +1,4 @@
+@echo off
+copy "C:\Users\Admin\.cursor\projects\c-Users-Admin-Desktop\assets\c__Users_Admin_AppData_Roaming_Cursor_User_workspaceStorage_c9d43f8befb31348021ecf1e7563cb23_images_Generated_image-a81d0252-718a-4415-ba9d-6e69db91f949.png" "icon-512.png"
+echo icon-512.png にコピーしました
+pause
